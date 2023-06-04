4845ad83a945a5e3137f496278671ee6779a9cd5
Alex Campos <alex.campos@gmail.com>
2023-06-04 13:15:50 -0500

    conditionally installing ansible

--- ansible.sh
+++ ansible.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+ansible_installed=$(dpkg -l | grep -c ansible)
+if [ "$ansible_installed" = "0" ]; then
+	sudo apt install -y software-properties-common
+	sudo add-apt-repository --yes --update ppa:ansible/ansible
+	sudo apt update
+	sudo apt install -y ansible
+fi
+
 sudo add-apt-repository universe -y
 #figure out how to test if the requirements are met before installing ansible collections
 # for example to check and install only for ansible community plugins
